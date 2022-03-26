48634177e8b31ad89af8b506c6cd44fcbcb7ca05
saifulhaque015 <saiful015.uiu@gmail.com>
2022-03-26 15:58:53 +0600

    final push

--- parameter.js
+++ parameter.js
@@ -1,5 +1,8 @@
 function bringSingara(taka) {
-    console.log(taka);
+    console.log('singara er jonno amake dise', taka);
+    var singaraPrice = 10;
+    var singaraQty = taka / singaraPrice;
+    return singaraQty;
 }
-
-bringSingara(20);
+var money = 20;
+bringSingara(money);
