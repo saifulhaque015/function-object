d79c4ba50d8ec79228661c3ba69c792c4214e2ff
saifulhaque015 <saiful015.uiu@gmail.com>
2022-03-26 14:56:25 +0600

    first commit

---
+++ function.js
@@ -0,0 +1,13 @@
+// declaring function
+function startFan() {
+    console.log('walk toward the switch');
+    console.log('press the switch');
+}
+
+// calling the function
+startFan();
+
+var city = 'dhaka';
+console.log(city);
+var location = 'nikunja-2';
+startFan();
